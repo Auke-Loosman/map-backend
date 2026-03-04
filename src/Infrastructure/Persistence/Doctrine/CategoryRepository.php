46372e87ca2a5db1e7fefb470a62cc457b841025
Auke <auke@skrepr.com>
2026-03-04 12:19:16 +0100

    feat(category): implement Doctrine CategoryRepository

---
+++ src/Infrastructure/Persistence/Doctrine/CategoryRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Doctrine;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Domain\Category\Entity\Category;
+use App\Domain\Category\Repository\CategoryRepositoryInterface;
+
+class CategoryRepository implements CategoryRepositoryInterface
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    public function saveCategory(Category $category): void
+    {
+        $this->entityManager->persist($category);
+        $this->entityManager->flush();
+    }
+
+    public function findCategoryById(int $id): ?Category
+    {
+        return $this->entityManager
+            ->getRepository(Category::class)
+            ->find($id);
+    }
+
+    public function findCategoriesByUserId(int $userId): array
+    {
+        return $this->entityManager
+            ->getRepository(Category::class)
+            ->findBy(['userId' => $userId]);
+    }
+}
