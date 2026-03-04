e7640539d625a2724eb99077b4eee49e20e4f224
Auke <auke@skrepr.com>
2026-03-04 11:42:35 +0100

    feat(user): add UserRepositoryInterface

---
+++ src/Domain/User/Repository/UserRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\User\Repository;
+
+use App\Domain\User\Entity\User;
+
+interface UserRepositoryInterface
+{
+    public function save(User $user): void;
+
+    public function findByEmail(string $email): ?User;
+
+    public function findById(int $id): ?User;
+}
