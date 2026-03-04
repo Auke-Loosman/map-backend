e0d2230c6d04f6d04cec1a05f1dba712ef1b6e93
Auke <auke@skrepr.com>
2026-03-04 12:49:16 +0100

    feat(item): add CreateItemCommand

---
+++ src/Application/Item/CreateItemCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Item;
+
+use Symfony\Component\Uid\Uuid;
+
+class CreateItemCommand
+{
+    public function __construct(
+        public string $name,
+        public string $description,
+        public Uuid $categoryId,
+        public ?float $latitude,
+        public ?float $longitude
+    ) {}
+}
