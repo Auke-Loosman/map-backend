40b05a5c89056325d636bddf710cfafb58d40133
Auke <auke@skrepr.com>
2026-03-04 12:42:52 +0100

    feat(category): add getCategories endpoint

--- src/UI/Http/Controller/CategoryController.php
+++ src/UI/Http/Controller/CategoryController.php
@@ -11,11 +11,13 @@ use Symfony\Component\Uid\Uuid;
 
 use App\Application\Category\CreateCategoryCommand;
 use App\Application\Category\CreateCategoryHandler;
+use App\Application\Category\GetCategoriesByUserHandler;
 
 class CategoryController
 {
     public function __construct(
-        private CreateCategoryHandler $handler
+        private CreateCategoryHandler $createCategoryHandler,
+        private GetCategoriesByUserHandler $getCategoriesHandler
     ) {}
 
     #[Route('/api/categories', methods: ['POST'])]
@@ -28,11 +30,29 @@ class CategoryController
             Uuid::fromString($data['userId'])
         );
 
-        $category = $this->handler->handle($command);
+        $category = $this->createCategoryHandler->handle($command);
 
         return new JsonResponse([
             'id' => $category->getId()->toRfc4122(),
             'name' => $category->getName()
         ], 201);
+    }
+
+    #[Route('/api/categories', methods: ['GET'])]
+    public function getCategories(Request $request): JsonResponse
+    {
+        $userId = $request->query->get('userId');
+
+        $categories = $this->getCategoriesHandler
+            ->handle(Uuid::fromString($userId));
+
+        $data = array_map(function ($category) {
+            return [
+                'id' => $category->getId()->toRfc4122(),
+                'name' => $category->getName()
+            ];
+        }, $categories);
+
+        return new JsonResponse($data);
     }
 }
